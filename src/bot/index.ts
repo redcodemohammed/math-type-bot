eba0f7a67a6d76393481dd7f9392bcb0a4577fd3
Mohammed <redcodemohammed@gmail.com>
2020-07-14 00:17:30 +0300

    remove commed

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -5,19 +5,23 @@ import svg2img from "svg2img";
 const { BOT_TOKEN } = process.env;
 const bot = new Telegraf(BOT_TOKEN);
 
-bot.command("math", async (ctx) => {
-	let math = ctx.message.text.split(" ").slice(1).join("");
+bot.on("text", async (ctx) => {
+	let math = ctx.message.text;
 	let svg = await math2svg(math);
 
-	svg2img(
-		svg,
-		{ quality: 100, width: 25 * math.length, height: 8 * math.length },
-		(err, buffer) => {
-			ctx.replyWithPhoto({
-				source: buffer
-			});
-		}
-	);
+	try {
+		svg2img(
+			svg,
+			{ quality: 100, width: 25 * math.length, height: 8 * math.length },
+			(err, buffer) => {
+				ctx.replyWithPhoto({
+					source: buffer
+				});
+			}
+		);
+	} catch {
+		ctx.reply("Please send a valid MathML");
+	}
 });
 
 bot.launch();
