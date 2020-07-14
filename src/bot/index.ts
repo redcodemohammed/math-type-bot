feb36384d3e3b1932b262cb6b73061611f17cca8
Mohammed <redcodemohammed@gmail.com>
2020-07-14 17:06:49 +0300

    added simplify command

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -2,6 +2,7 @@ import { Telegraf } from "telegraf";
 
 import math from "./commands/math";
 import inline from "./commands/inline";
+import simplify from "./commands/simplify";
 
 const { BOT_TOKEN } = process.env;
 const bot = new Telegraf(BOT_TOKEN);
@@ -11,6 +12,8 @@ bot.help((ctx) => {
 		"Please read this to learn how to use the bot http://asciimath.org/#syntax"
 	);
 });
+
+bot.command("simplify", simplify);
 
 bot.on("inline_query", inline);
 bot.on("text", math);
