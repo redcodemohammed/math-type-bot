c10101cfac7578b37b1905dc8e819f115a2a22ff
Mohammed <redcodemohammed@gmail.com>
2020-07-15 10:39:20 +0300

    correct help msg

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -11,12 +11,12 @@ const { BOT_TOKEN } = process.env;
 const bot = new Telegraf(BOT_TOKEN);
 
 bot.help((ctx) => {
-	ctx.reply(
+	ctx.replyWithMarkdown(
 		"Here is list of the available commands:\n" +
-			"- sim <expression> to simplify that expression.\n" +
-			"- int <expression> to find the indefinite integral. (some math function are not supported such as sec and csc).\n" +
-			"\n- der <expression> to find the derivative.\n" +
-			"- slo <math equation> to solve it.\n" +
+			"- ```/sim <expression>``` to simplify that expression.\n" +
+			"- ```/int <expression>``` to find the indefinite integral. (some math function are not supported such as sec and csc).\n" +
+			"\n- ```/der <expression>``` to find the derivative.\n" +
+			"- ```/sol <math equation>``` to solve it.\n" +
 			"Please read this to learn how to write the expression http://asciimath.org/#syntax`"
 	);
 });
